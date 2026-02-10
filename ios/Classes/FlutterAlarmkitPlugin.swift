7632e4aa6295eb2623c521cc44a18ca8c1ccf98b
koodimetsa <make.metsa@gmail.com>
2026-02-10 10:49:51 +0200

    Update FlutterAlarmkitPlugin.swift

--- ios/Classes/FlutterAlarmkitPlugin.swift
+++ ios/Classes/FlutterAlarmkitPlugin.swift
@@ -21,9 +21,9 @@ public class FlutterAlarmkitPlugin: NSObject, FlutterPlugin {
     let instance = FlutterAlarmkitPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
 
-    let eventChannel = FlutterEventChannel(name: "flutter_alarmkit/events", binaryMessenger: registrar.messenger())
+    /*let eventChannel = FlutterEventChannel(name: "flutter_alarmkit/events", binaryMessenger: registrar.messenger())
     let streamHandler = AlarmUpdateStreamHandler()
-    eventChannel.setStreamHandler(streamHandler)
+    eventChannel.setStreamHandler(streamHandler)*/
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
